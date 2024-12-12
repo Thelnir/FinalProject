7fcdd25f62a41351601f5b785d6819dd7a2045c3
Thelnir <jluckymissychristian@gmail.com>
2024-12-12 02:05:05 -0600

    Add files via upload

---
+++ Final project/script.js
@@ -0,0 +1,47 @@
+/*
+Student Name: Joshua Whitmore
+Today's Date: 12/9/2024
+File Name: styles.css
+*/
+
+var answer = document.querySelector("#answer p");
+var heading = document.querySelector("#answer h2");
+
+//Hamburger menu function
+function hamburger() 
+{
+    var navlinks = document.getElementById("nav-links");
+    var menuicon = document.getElementById("icon");
+    if (navlinks.style.display === "block")
+    {
+        navlinks.style.display = "none";
+        menuicon.style.color = "#0047AB"
+    }
+    else
+    {
+        navlinks.style.display = "block";
+        menuicon.style.color = "#f6eee4"
+    }
+}
+//Function to display the first answer
+function ans1()
+{
+    heading.style.display = "block";
+    answer.textContent ="Computers are not the only things in our lives that can be broken down into the same three components.";
+    var nextQ=document.querySelector(".linear #Q2");
+    nextQ.style.display="block";
+}
+function ans2()
+{
+    heading.style.display = "block";
+    answer.textContent ="Think about it: what are the most basic organs of a computer? What fundamental systems do they require to function?";
+    var nextQ=document.querySelector(".linear #Q3");
+    nextQ.style.display="block";
+}
+function ans3()
+{
+    heading.style.display = "block";
+    answer.textContent ="Energy to perform an action, code to tell the energy what to do, and a processor to run the code. These three components make up everything around us. See the next page for examples.";
+    var nextQ=document.querySelector(".linear #ans");
+    nextQ.style.display="block";
+}
