b2b25855528353e1d9b48e1ace1280ac5eabf95c
Thelnir <jluckymissychristian@gmail.com>
2024-12-13 22:24:05 -0600

    Add files via upload

--- Final project/script.js
+++ Final project/script.js
@@ -6,6 +6,7 @@ File Name: styles.css
 
 var answer = document.querySelector("#answer p");
 var heading = document.querySelector("#answer h2");
+var nextQ = document.querySelector("#Q2");
 
 //Hamburger menu function
 function hamburger() 
@@ -28,20 +29,19 @@ function ans1()
 {
     heading.style.display = "block";
     answer.textContent ="Computers are not the only things in our lives that can be broken down into the same three components.";
-    var nextQ=document.querySelector(".linear #Q2");
     nextQ.style.display="block";
 }
 function ans2()
 {
     heading.style.display = "block";
+    nextQ = document.querySelector("#Q3");
     answer.textContent ="Think about it: what are the most basic organs of a computer? What fundamental systems do they require to function?";
-    var nextQ=document.querySelector(".linear #Q3");
-    nextQ.style.display="block";
+    nextQ.style.display = "block";
 }
 function ans3()
 {
     heading.style.display = "block";
-    answer.textContent ="Energy to perform an action, code to tell the energy what to do, and a processor to run the code. These three components make up everything around us. See the next page for examples.";
-    var nextQ=document.querySelector(".linear #ans");
-    nextQ.style.display="block";
+    nextQ=document.querySelector("#ans");
+    answer.textContent = "Energy to perform an action, code to tell the energy what to do, and a processor to run the code. These three components make up everything around us. See the next page for examples.";
+    nextQ.style.display = "block";
 }
